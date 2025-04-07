1e91230d97a78cf692b1c820e0c5be3b1c593623
Astro Xu <astroxu24@outlook.com>
2025-04-07 10:37:31 +0800

    优化代码

--- script/mongodb_inspector.py
+++ script/mongodb_inspector.py
@@ -278,24 +278,24 @@ SUMMARY_HTML_TEMPLATE= '''
 class CheckItem:
     # 定义检查项名称到方法名的映射
     METHOD_MAP = {
-        "MONGODB_VERSION_CHECK": "mongodb_version_check",
-        "TOP_5_DATABASES_SIZE_CHECK": "top_5_databases_size_check",
-        "COLLECTION_STATS_CHECK": "collection_stats_check",
-        "SERVER_UPTIME_CHECK": "server_uptime_check",
-        "MEMORY_USAGE_CHECK": "memory_usage_check",
-        "OPCOUNTERS_CHECK": "opcounters_check",
-        "CONNECTIONS_CHECK": "connections_check"
+        "MONGODB_VERSION_CHECK": "check_mongodb_version",
+        "TOP_5_DATABASES_SIZE_CHECK": "check_top_5_databases_size",
+        "COLLECTION_STATS_CHECK": "check_collection_stats",
+        "SERVER_UPTIME_CHECK": "check_server_uptime",
+        "MEMORY_USAGE_CHECK": "check_memory_usage",
+        "OPCOUNTERS_CHECK": "check_opcounters",
+        "CONNECTIONS_CHECK": "check_connections"
     }
 
-    def __init__(self, name, expected_value, check_type, exec_func, remark, is_cloud_check = None):
+    def __init__(self, name, expected_value, check_type, exec_func, remark, is_cloud_check=None):
         self.name = name
         self.remark = remark
         self.exec_func = self.METHOD_MAP.get(exec_func)
         self.expected_value = expected_value
         self.check_type = check_type
-        self.is_cloud_check = is_cloud_check  # 新增字段
+        self.is_cloud_check = is_cloud_check  # 是否云数据库的检查项 None: 云和非云数据库都需要的检查项 True: 云数据库检查项 False: 非云数据库检查项
 
-    def execute(self, client,db_name,check_group):
+    def execute(self, client, db_name, check_group):
         try:
             method = getattr(self, self.exec_func)
             actual_result = method(client)
@@ -306,14 +306,11 @@ class CheckItem:
             elif self.check_type == 'threshold':
                 actual_value = float(actual_result)
                 expected_value = float(self.expected_value)
-                # if comparison == 'greater_than':
-                #     status = 'Passed' if actual_value > expected_value else 'Failed'
-                # else:
-                #     status = 'Failed'
+                # 补充阈值比较逻辑（示例：默认大于）
+                status = 'Passed' if actual_value > expected_value else 'Failed'
             else:
                 status = 'Failed'
 
-            # 记录结果
             return {
                 'name': self.name,
                 'db_name': db_name,
@@ -332,10 +329,12 @@ class CheckItem:
                 'expected_result': self.expected_value
             }
 
-    def mongodb_version_check(self, client):
+    def check_mongodb_version(self, client):
+        # 检查 MongoDB 服务器的版本
         return client.admin.command('serverStatus')['version']
 
-    def top_5_databases_size_check(self, client):
+    def check_top_5_databases_size(self, client):
+        # 检查前 5 大数据库的大小
         databases = client.list_database_names()
         database_sizes = []
         for db_name in databases:
@@ -357,12 +356,8 @@ class CheckItem:
         sorted_databases = sorted(database_sizes, key=lambda x: x['数据大小'], reverse=True)
         return sorted_databases[:5]
 
-    '''
-    节点角色状态：
-    var a = rs.status(); 
-    a.members.forEach(function(e){print(e.name, e.stateStr)})
-    '''
-    def collection_stats_check(self, client):
+    def check_collection_stats(self, client):
+        # 检查每个集合的存储、数据和索引大小
         # 执行 rs.status() 命令
         rs_status = client.admin.command('replSetGetStatus')
 
@@ -375,16 +370,20 @@ class CheckItem:
             db_stats.append({ 'name': member.get('name'), 'stats':  member.get('stateStr') })
         return db_stats
 
-    def server_uptime_check(self, client):
+    def check_server_uptime(self, client):
+        # 检查 MongoDB 服务器的运行时间
         return client.admin.command('serverStatus')['uptime']
 
-    def memory_usage_check(self, client):
+    def check_memory_usage(self, client):
+        # 检查 MongoDB 服务器的内存使用情况
         return client.admin.command('serverStatus')['mem']
 
-    def opcounters_check(self, client):
+    def check_opcounters(self, client):
+        # 检查执行的操作数量（插入、查询、更新、删除、命令）
         return client.admin.command('serverStatus')['opcounters']
 
-    def connections_check(self, client):
+    def check_connections(self, client):
+        # 检查当前连接到 MongoDB 服务器的连接数
         return client.admin.command('serverStatus')['connections']
 
 # 定义检查组类
@@ -397,7 +396,7 @@ class CheckGroup:
     def perform_checks(self, db_name, client):
         results = {}
         for check in self.checks:
-            result = check.execute(client,db_name,self.name)
+            result = check.execute(client, db_name, self.name)
             results[f"{check.name}"] = result
         return results
 
@@ -409,8 +408,8 @@ def perform_checks(db_name, client, check_groups, checks_to_run, is_cloud):
             group_results = {}
             for check in check_group.checks:
                 if (check.is_cloud_check is None) or \
-                   (check.is_cloud_check  and is_cloud) or \
-                   (not check.is_cloud_check  and not is_cloud):
+                   (check.is_cloud_check and is_cloud) or \
+                   (not check.is_cloud_check and not is_cloud):
                     result = check.execute(client, db_name, check_group_name)
                     group_results[f"{check.name}"] = result
             results_by_group[check_group_name] = group_results
@@ -440,8 +439,11 @@ def generate_html_report(results_by_group, output_path, db_name):
         for result in group_results.values():
             result['actual_result'] = format_result(result['actual_result'])
 
+    # 过滤掉没有检查项的检查组
+    filtered_results_by_group = {k: v for k, v in results_by_group.items() if v}
+
     template = Template(DETAIL_HTML_TEMPLATE)
-    html_content = template.render(results_by_group=results_by_group, db_name=db_name, total_checks=total_checks, passed_checks=passed_checks, failed_checks=failed_checks, error_checks=error_checks)
+    html_content = template.render(results_by_group=filtered_results_by_group, db_name=db_name, total_checks=total_checks, passed_checks=passed_checks, failed_checks=failed_checks, error_checks=error_checks)
     with open(output_path, 'w', encoding='utf-8') as file:
         file.write(html_content)
 
@@ -452,25 +454,30 @@ def generate_summary_html_report(results, output_path, config):
     # 获取数据库名和文件相对地址链接
     db_links = {db_config['name']: f"{db_config['name'].replace(' ', '_')}_report.html" for db_config in config['databases']}
 
+    # 修正总检查项数统计方式
+    total_checks = len([r for r in results if r[1]['status'] in ['Passed', 'Failed', 'Error']])
+
     # 统计成功项数、错误项数以及总项数
-    total_checks = len(results)
-    passed_checks = sum(1 for k,result in results if result['status'] == 'Passed')
-    failed_checks = sum(1 for k,result in results if result['status'] == 'Failed')
-    error_checks = sum(1 for k,result in results if result['status'] == 'Error')  # 新增统计Error项数
+    passed_checks = sum(1 for k, result in results if result['status'] == 'Passed')
+    failed_checks = sum(1 for k, result in results if result['status'] == 'Failed')
+    error_checks = sum(1 for k, result in results if result['status'] == 'Error')  # 新增统计Error项数
 
     # 按数据库分组统计
     db_results = {}
     for db_config in config['databases']:
         db_name = db_config['name']
-        db_results[db_name]= {f'{name}_{result["check_group"]}': result for name, result in results if db_name == result['db_name']}
+        db_results[db_name] = {f'{name}_{result["check_group"]}': result for name, result in results if db_name == result['db_name']}
 
     # 格式化实际结果
     for db_name, results in db_results.items():
         for result in results.values():
             result['actual_result'] = format_result(result['actual_result'])
 
+    # 过滤掉没有检查项的数据库结果
+    filtered_db_results = {k: v for k, v in db_results.items() if v}
+
     template = Template(SUMMARY_HTML_TEMPLATE)
-    html_content = template.render(results=results, db_links=db_links, total_checks=total_checks, passed_checks=passed_checks, failed_checks=failed_checks, error_checks=error_checks, config=config, db_results=db_results)  # 更新模板渲染参数
+    html_content = template.render(results=results, db_links=db_links, total_checks=total_checks, passed_checks=passed_checks, failed_checks=failed_checks, error_checks=error_checks, config=config, db_results=filtered_db_results)  # 更新模板渲染参数
     with open(output_path, 'w', encoding='utf-8') as file:
         file.write(html_content)
 
@@ -502,10 +509,8 @@ def main():
         uri = db_config['uri']
         db_name = db_config['name']
         is_cloud = db_config['is_cloud']  # 获取是否为云数据库的配置
-        client = MongoClient(uri, server_api=ServerApi('1'))
-
-        checks_to_run = check_groups.keys()
-        db_results_by_group = perform_checks(db_name, client, check_groups, checks_to_run, is_cloud)  # 传递 is_cloud 参数
+        with MongoClient(uri, server_api=ServerApi('1')) as client:
+            db_results_by_group = perform_checks(db_name, client, check_groups, checks_to_run, is_cloud)
 
         # 生成每个数据库的详细报告
         db_report_path = os.path.join(new_output_report_dir, f'{db_name.replace(" ", "_")}_report.html')
@@ -514,8 +519,6 @@ def main():
         # 汇总结果
         for group_results in db_results_by_group.values():
             all_results.extend(group_results.items())
-
-        client.close()
 
     # 生成汇总报告
     generate_summary_html_report(all_results, os.path.join(new_output_report_dir, 'summary_report.html'), config)
@@ -527,18 +530,18 @@ def main():
 
 # 初始化检查组
 check_groups = {
-    "database_performance":  CheckGroup("数据库性能", "与数据库性能和配置相关的检查。",[
-        CheckItem("MongoDB 版本检查", "", "output_contains","MONGODB_VERSION_CHECK","检查 MongoDB 服务器的版本。", is_cloud_check=None),
-        CheckItem("前 5 大数据库大小检查", "", "output_contains","TOP_5_DATABASES_SIZE_CHECK","检查前 5 大数据库的大小。", is_cloud_check=None),
-        CheckItem("集合统计信息检查",  "", "output_contains","COLLECTION_STATS_CHECK","检查每个集合的存储、数据和索引大小。", is_cloud_check=None),
-        CheckItem("服务器运行时间检查", "", "output_contains","SERVER_UPTIME_CHECK","检查 MongoDB 服务器的运行时间。", is_cloud_check=None),
-        CheckItem("内存使用情况检查",  "", "output_contains","MEMORY_USAGE_CHECK","检查 MongoDB 服务器的内存使用情况。", is_cloud_check=None),
-        CheckItem("操作计数器检查",  "", "output_contains","OPCOUNTERS_CHECK","检查执行的操作数量（插入、查询、更新、删除、命令）。", is_cloud_check=None),
-        CheckItem("连接数检查", "", "output_contains","CONNECTIONS_CHECK","检查当前连接到 MongoDB 服务器的连接数。", is_cloud_check=False)
+    "database_performance": CheckGroup("数据库性能", "与数据库性能和配置相关的检查。",[
+        CheckItem("MongoDB 版本检查", "", "output_contains","MONGODB_VERSION_CHECK","检查 MongoDB 服务器的版本。", None),
+        CheckItem("前 5 大数据库大小检查", "", "output_contains","TOP_5_DATABASES_SIZE_CHECK","检查前 5 大数据库的大小。", None),
+        CheckItem("集合统计信息检查",  "", "output_contains","COLLECTION_STATS_CHECK","检查每个集合的存储、数据和索引大小。", True),  # 明确设置云环境检查
+        CheckItem("服务器运行时间检查", "", "output_contains","SERVER_UPTIME_CHECK","检查 MongoDB 服务器的运行时间。", None),
+        CheckItem("内存使用情况检查",  "", "output_contains","MEMORY_USAGE_CHECK","检查 MongoDB 服务器的内存使用情况。", None),
+        CheckItem("操作计数器检查",  "", "output_contains","OPCOUNTERS_CHECK","检查执行的操作数量（插入、查询、更新、删除、命令）。", None),
+        CheckItem("连接数检查", "", "output_contains","CONNECTIONS_CHECK","检查当前连接到 MongoDB 服务器的连接数。", False)
     ]),
     "database_performance2": CheckGroup("数据库性能2", "与数据库性能和配置相关的检查。", [
-        CheckItem("操作计数器检查", "", "output_contains","OPCOUNTERS_CHECK","检查执行的操作数量（插入、查询、更新、删除、命令）。", is_cloud_check=True),
-        CheckItem("连接数检查",  "", "output_contains","CONNECTIONS_CHECK","检查当前连接到 MongoDB 服务器的连接数。", is_cloud_check=True)
+        CheckItem("操作计数器检查", "", "output_contains","OPCOUNTERS_CHECK","检查执行的操作数量（插入、查询、更新、删除、命令）。", True),
+        CheckItem("连接数检查",  "", "output_contains","CONNECTIONS_CHECK","检查当前连接到 MongoDB 服务器的连接数。", True)
     ])
 }
 
